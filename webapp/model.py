f6f9367e1f33482412efb0591c84483f81433558
Matteo Possamai <matteopossamai@localhost.localdomain>
2021-09-21 16:05:52 +0200

    Added the base for graph, started to handle datas to put there. Need to finish to handle them, and put into the graph

--- webapp/model.py
+++ webapp/model.py
@@ -1,3 +1,4 @@
+from sqlalchemy.orm import defaultload
 from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
@@ -6,4 +7,4 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key = True )
     name = db.Column(db.String(100), unique = True )
     password = db.Column(db.String(100))
-    weightStrory = db.Column(db.String(10000))
+    weightStrory = db.Column(db.String(10000), default = "")
--- webapp/view.py
+++ webapp/view.py
@@ -1,5 +1,9 @@
 from flask import Blueprint, render_template, session, redirect, url_for, request
+from flask.helpers import flash
 from flask_login import current_user, login_required
+from .model import User
+from . import db
+import json
 
 view = Blueprint("view", __name__)
 
@@ -18,4 +22,27 @@ def healthy():
 @view.route("/journey", methods=["GET", "POST"])
 @login_required
 def journey():
-    return render_template("journey.html", user = current_user)
+    if request.method == "POST":
+        newWeight = request.form.get("value")
+        try: 
+            newWeight = int(newWeight)
+            current_user.weightStrory += "&" + str(newWeight)
+            db.session.commit()
+        except:
+            flash("Invalid input", category="error")
+    
+    data = {}
+    ws = current_user.weightStrory
+    if ws:
+        i = 0
+        ind1 = 0
+        ind2 = 0
+        arr = []
+        while i<len(ws):
+            #finish here the code
+            #https://stackoverflow.com/questions/65359530/passing-json-data-from-flask-to-javascript
+            if ws[i] == "&":
+                ind1 = i
+            else: 
+                ind2 += 1
+    return render_template("journey.html", user = current_user, data = None)
